12881383e25715b111fcaf1848cec8c01f821a14
kaiquemaiaa <126829998+kaiquemaiaa@users.noreply.github.com>
2023-06-05 17:40:31 -0300

    Add files via upload

---
+++ GLOBAL SOLUTION/funcionalidades/darkmode.js
@@ -0,0 +1,5 @@
+
+var darkModeButton = document.getElementById("darkModeButton");
+darkModeButton.addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
---
+++ GLOBAL SOLUTION/funcionalidades/hora.js
@@ -0,0 +1,26 @@
+
+function atualizarDataHora() {
+  // Cria um novo objeto Date
+  var dataHoraAtual = new Date();
+
+  // Obtém os componentes da data e da hora
+  var dia = dataHoraAtual.getDate();
+  var mes = dataHoraAtual.getMonth() + 1; // Os meses em JavaScript são baseados em zero
+  var ano = dataHoraAtual.getFullYear();
+  var hora = dataHoraAtual.getHours();
+  var minutos = dataHoraAtual.getMinutes();
+  var segundos = dataHoraAtual.getSeconds();
+
+  // Formata a data e a hora no formato desejado (exemplo: "DD/MM/AAAA HH:MM:SS")
+  var dataHoraFormatada = dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minutos + ':' + segundos;
+
+  // Atualiza o elemento HTML com a data e a hora
+  document.getElementById('data-hora').textContent = dataHoraFormatada;
+}
+
+// Chama a função inicialmente para exibir a data e a hora
+atualizarDataHora();
+
+// Atualiza a data e a hora a cada segundo
+setInterval(atualizarDataHora, 1000);
+  
